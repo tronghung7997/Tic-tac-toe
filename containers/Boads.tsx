4483820d3a24a6b5ed12f4226d175b0c032f8b7e
Trong Hung <thtm79@gmail.com>
2022-03-07 08:17:51 +0700

    Update logic game

--- containers/Boads.tsx
+++ containers/Boads.tsx
@@ -1,48 +1,59 @@
-import { useEffect, useState } from "react"
-import Square from "../components/Square"
-import styled from "styled-components"
+import { useEffect, useState } from "react";
+import Square from "../components/Square";
+import styled from "styled-components";
 
-type Player = 'X' | 'O' | 'BOTH' | null
-
+type Player = "X" | "O" | "BOTH" | null;
 
 function Board() {
+    const [tempBoardSize, setTempBoardSize] = useState(null);
+    const [boardSize, setBoardSize] = useState(null);
+    const [startGame, setStartGame] = useState(false);
 
-    const [rows, setRows] = useState(0)
-    const [squares, setSquares] = useState(Array(rows*rows).fill(null))
-    const [currentPlayer, setCurrentPlayer] = useState<'X' | 'O'>(
-        Math.round(Math.random()) === 1 ? 'X' : 'O'
-    )
-    const [winner, setWinner] = useState(null)
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        setBoardSize(tempBoardSize);
+        setStartGame(true);
+    }
+
+    const [rows, setRows] = useState(0);
+    const [squares, setSquares] = useState(Array(rows * rows).fill(null));
+    const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
+        Math.round(Math.random()) === 1 ? "X" : "O"
+    );
+    const [winner, setWinner] = useState(null);
+
+    const createBoard = (n: number) => {
+        return [...Array(n)].map((row) => Array(n).fill(null));
+    };
 
     const Grid = styled.div`
-        display: grid;
-        grid-gap: 1px;
-        grid-template-columns: repeat(${props=>props.rows}, 1fr);
-    `
+    display: grid;
+    grid-gap: 1px;
+    grid-template-columns: repeat(${(props) => props.rows}, 1fr);
+  `;
 
     function setSquaresValue(index: number) {
         const newData = squares.map((val, i) => {
             if (i === index) {
-                return currentPlayer
+                return currentPlayer;
             }
-            return val
-        })
-        setSquares(newData)
-        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
+            return val;
+        });
+        setSquares(newData);
+        setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
     }
 
-    function reset(){
-        setSquares(Array(rows*rows).fill(null))
-        setWinner(null)
-        setCurrentPlayer(Math.round(Math.random()) === 1 ? 'X' : 'O')
+    function reset() {
+        setSquares(Array(rows * rows).fill(null));
+        setWinner(null);
+        setCurrentPlayer(Math.round(Math.random()) === 1 ? "X" : "O");
     }
 
-    function handleOption(event:any){
-        setRows(event.target.value)
+    function handleOption(event: any) {
+        setRows(event.target.value);
     }
-    
 
-    function calWinner(squares: Player[]){
+    function calWinner(squares: Player[]) {
         const lines = [
             [0, 1, 2],
             [3, 4, 5],
@@ -52,52 +63,85 @@ function Board() {
             [2, 5, 8],
             [0, 4, 8],
             [2, 4, 6],
-        ]
-        for(let i = 0; i < lines.length ; i++){
-            const [a, b, c] = lines[i]
-            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-                return squares[a]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (
+                squares[a] &&
+                squares[a] === squares[b] &&
+                squares[a] === squares[c]
+            ) {
+                return squares[a];
             }
         }
-        return null
+        return null;
     }
 
-    useEffect(() => {
-        const w = calWinner(squares)
-        if(w){
-            setWinner(w)
-        }
-    })
-    return <div>
-        Please Select quantity of rows
-        <select id="cars" onChange = {handleOption}>
-            <option value=""></option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-        </select>
+    // useEffect(() => {
+    //     const w = calWinner(squares);
+    //     if (w) {
+    //         setWinner(w);
+    //     }
+    // });
 
-        {rows && 
-            <>
-                {<p>{currentPlayer} turn</p>}
-                {winner && <p>{winner} WIN</p>}
-                <Grid rows={rows}>
-                    {
-                        Array(rows*rows).fill(null).map((_, i) => {
-                            return <Square
-                                winner={winner}
-                                key={i}
-                                onClick={() => setSquaresValue(i)}
-                                value={squares[i]}
-                            />
-                        })
-                    }
-                </Grid>
-                <button className="reset" onClick={reset}>Reset</button>
-            </>
-        }        
+    return (
+        <div>
+            {/* Please Select quantity of rows
+            <select id="cars" onChange={handleOption}>
+                <option value=""></option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            {rows && (
+                <>
+                    {<p>{currentPlayer} turn</p>}
+                    {winner && <p>{winner} WIN</p>}
+                    <Grid rows={rows}>
+                        {Array(rows * rows)
+                            .fill(null)
+                            .map((_, i) => {
+                                return (
+                                    <Square
+                                        winner={winner}
+                                        key={i}
+                                        onClick={() => setSquaresValue(i)}
+                                        value={squares[i]}
+                                    />
+                                );
+                            })}
+                    </Grid>
+                    <button className="reset" onClick={reset}>
+                        Reset
+                    </button>
+                </>
+            )} */}
 
-    </div>
+            <div>
+                {startGame ? (
+                    <div className="game-board">
+                        <Board boardSize={boardSize} />
+                    </div>
+                ) : (
+                    <p>Choose board size to start game</p>
+                )}
+                <label>Enter board size: </label>
+                <input
+                    type="text"
+                    placeholder="Enter a number"
+                    id="board-size"
+                    onChange={(e) => setTempBoardSize(e.target.value)}
+                />
+                <button
+                    type="submit"
+                    className="button"
+                    onClick={(e) => handleSubmit(e)}
+                >
+                    Start Game
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default Board
+export default Board;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Board from '../containers/Boads'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
+import Board from "../components/Boads";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [tempBoardSize, setTempBoardSize] = useState(null);
+  const [boardSize, setBoardSize] = useState(null);
+  const [startGame, setStartGame] = useState(false);
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    setBoardSize(tempBoardSize);
+    setStartGame(true);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,10 +24,33 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Board></Board>
+        <div>
+          {startGame ? (
+            <div className="game-board">
+              <Board boardSize={boardSize} />
+            </div>
+          ) : (
+            <p>Choose board size to start game</p>
+          )}
+          <label>Enter board size: </label>
+          <input
+            type="text"
+            placeholder="Enter a number"
+            id="board-size"
+            onChange={(e) => setTempBoardSize(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="button"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Start Game
+          </button>
+        </div>
+        {/* <Board></Board> */}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
